48050be0b66ec1e625e172f2126097ae946164da
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-08 13:45:12 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,20 @@
 class SongsController < ApplicationController
 
 def index
+  @songs = Song.all
 end
 
 def show
+  @song = Song.find_by(params[:id])
 end
 
 def create
+  @song = Song.new
+  if @song.save
+    redirect_to @song
+  else
+    render :edit
+end
 end
 
 def update
@@ -16,4 +24,7 @@ def edit
 end
 
 def destroy
+end
+
+
 end
