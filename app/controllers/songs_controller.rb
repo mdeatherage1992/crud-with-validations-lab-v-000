4a2788fa65b92285de89f0f0f3cf9602c587dd6b
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-08 13:47:47 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,10 +27,22 @@ def update
 end
 
 def edit
+  @song = Song.find(params[:id])
 end
 
 def destroy
+  @song = Song.find(params[:id])
+  @song.destroy
+  redirect_to songs_url
 end
+
+private
+
+def song_params
+   params.require(:song).permit(
+     :title, :release_year, :released, :genre, :artist_name
+   )
+
 
 
 end
