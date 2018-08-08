91024142951bca83804e3d8df2bb96f6b6d5ba47
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-08 13:54:33 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,7 +8,7 @@ class Song < ActiveRecord::Base
   validates :artist_name, presence: true
 
   with_options if: :released? do |song|
-    song.validates :released_year, presence: true
+    song.validates :release_year, presence: true
     song.validates :release_year, numericality: {
       less_than_or_equal_to: Date.today.year
     }
