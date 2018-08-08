cedf6ec661d2a0595a47b3a645c029b316a7397b
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-08 13:53:58 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :title, uniqeness: {
+  validates :title, uniqueness: {
     scope: [:release_year,:artist_name],
     message: "cannot be repeated by the same artist in the same year"
   }
